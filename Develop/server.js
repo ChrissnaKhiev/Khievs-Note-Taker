4b237c36d80b6bf655bf11d347d04a7d046ce025
Chris Khiev <chrissnakhiev@gmail.com>
2022-10-18 01:26:49 -0400

    added uuid

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 const notes = require('./db/db.json');
 const PORT = 3001;
 const app = express();
@@ -9,6 +10,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// const writeToFile = (destination, content) => 
+// fs.writeFile(destination, JSON.stringify(content), (err) => err
+// ? console.error(err)
+// : console.log(
+//     `Note for ${newNote.title} has been written to JSON file`)
+// );
+
+// const readAndAppend = (content, file) => {
+//     fs.readFile(file, 'utf8', (err,data) => {
+//         if (err) {
+//             console.log(err);
+//         }else {
+//             const parseData = JSON.parse(data);
+//             parseData.push(content);
+//             writeToFile(file, parseData);
+//         }
+//     })
+// }
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -19,11 +39,13 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to get reviews`)
+    res.json(`${req.method} request received to get notes`)
+    console.info(`${req.method} request received for notes`);
 });
 
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to get reviews`);
+    res.json(`${req.method} request received to post notes`);
+    console.info(`${req.method} request received for notes`);
 
     const { title, text} = req.body;
 
@@ -31,11 +53,12 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
+            note_id: uuid(),
         };
 
         const noteString = JSON.stringify(newNote);
 
-        fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) => err
+        fs.writeFile(`./db/db.json`, noteString, (err) => err
         ? console.error(err)
         : console.log(
             `Note for ${newNote.title} has been written to JSON file`)
@@ -49,7 +72,7 @@ app.post('/api/notes', (req, res) => {
         console.log(response);
         res.status(201).json(response);
     }else {
-        res.status(500).json('Error in posting review');
+        res.status(500).json('Error in posting note');
     }
 });
 
